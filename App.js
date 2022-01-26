e2b28cb4f88973ebfcf3df0ad2a2b3be1b18612a
Jin Park <jinpark1504@gmail.com>
2022-01-25 22:45:54 -0500

    style

--- App.js
+++ App.js
@@ -1,13 +1,52 @@
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{ flex: 1, padding: 15 }}>
-      {/* there is no other sibling. */}
-      <View style={{ flex: 1, backgroundColor: 'tomato' }}></View>
-      <View style={{ flex: 2, backgroundColor: 'green' }}></View>
-      <View style={{ flex: 1, backgroundColor: 'gold' }}></View>
+    <View style={styles.container}>
+      <StatusBar style='dark'></StatusBar>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'violet',
+  },
+  city: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cityName: {
+    fontSize: 46,
+    fontWeight: '600',
+  },
+  weather: {
+    flex: 3,
+  },
+  day: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 100,
+    fontWeight: '700',
+  },
+  description: {
+    marginTop: -20,
+    fontSize: 40,
+  },
+});
--- README.md
+++ README.md
@@ -128,3 +128,60 @@
       );
     }
     ```
+
+## Style
+
+- ```jsx
+  import { StatusBar } from 'expo-status-bar';
+  import React from 'react';
+  import { View, Text, StyleSheet } from 'react-native';
+
+  export default function App() {
+    return (
+      <View style={styles.container}>
+        <StatusBar style='dark'></StatusBar>
+        <View style={styles.city}>
+          <Text style={styles.cityName}>Seoul</Text>
+        </View>
+        <View style={styles.weather}>
+          <View style={styles.day}>
+            <Text style={styles.temp}>27</Text>
+            <Text style={styles.description}>Sunny</Text>
+          </View>
+        </View>
+      </View>
+    );
+  }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: 'violet',
+    },
+    city: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    cityName: {
+      fontSize: 46,
+      fontWeight: '600',
+    },
+    weather: {
+      flex: 3,
+    },
+    day: {
+      flex: 1,
+      alignItems: 'center',
+    },
+    temp: {
+      marginTop: 50,
+      fontSize: 100,
+      fontWeight: '700',
+    },
+    description: {
+      marginTop: -20,
+      fontSize: 40,
+    },
+  });
+  ```
